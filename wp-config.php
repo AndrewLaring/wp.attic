f49bffacd5d047e7cd5be6cad79796354248724c
AndrewLaring <larinoffski@gmail.com>
2016-10-27 16:23:41 +0300

    CHANGE HTTP TO SSH

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'wp_attic');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'labuelle');
+define('DB_PASSWORD', '23');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
